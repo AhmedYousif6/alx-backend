eff6f6a2c323bf5c2b5489c50019b8bf29f4123e
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-08-22 03:27:37 +0100

    fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -27,6 +27,8 @@ class FIFOCache(BaseCaching):
         if key is not None and item is not None:
             if key not in self.cache_data:
                 self.order_list.append(key)
+            self.order_list.remove(key)
+            self.order_list.append(key)
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 first_in = self.order_list.pop(0)
@@ -38,4 +40,6 @@ class FIFOCache(BaseCaching):
         return the value linked to the given key in
         the cache_data
         """
+        if key is None or key not in self.cache_data:
+            return None
         return self.cache_data.get(key, None)
